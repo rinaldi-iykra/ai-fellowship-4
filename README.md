7f89967a00602d9d7d1f0d4c3615a9c81b751cf4
Rinaldi <rinaldiamfine@gmail.com>
2025-02-24 10:17:38 +0700

    Empty the repo

--- README.md
+++
@@ -1,1 +0,0 @@
-https://ai-dashboard-app-run-791022737244.us-central1.run.app
--- project/app/dashboard/__init__.py
+++
@@ -1,7 +0,0 @@
-from project.app.dashboard.stock_optim_table import *
-from project.app.dashboard.stock_pivot_table import *
-from project.app.dashboard.stockout import *
-from project.app.dashboard.overstock_cost import *
-from project.app.dashboard.itr import *
-from project.app.dashboard.fct_vs_act import *
-from project.app.dashboard.filter import *
